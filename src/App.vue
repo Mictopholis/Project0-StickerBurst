ba91e94f3d3287c7f83450a3c5d250d4d26f8c21
MeeraWakim <meera.wakim925@gmail.com>
2018-09-12 12:32:49 -0500

    created vue framework including all components

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,34 @@
 <template>
-<v-app>
-    <v-toolbar>
-    Testing the toolbar
-    <v-icon>home</v-icon>
-    </v-toolbar>
-</v-app>
+    <v-app>
+        <v-toolbar>
+            <v-icon>home</v-icon>
+            <h1>Logo</h1>
+            <v-spacer/>
+            <Login/>
+        </v-toolbar>
+
+        <v-container grid-list-md text-xs-center>
+            <v-layout row wrap>
+                <v-flex xs12>
+                    <v-card>
+                        <h1 align="center">Informational Image</h1>
+                    </v-card>
+                </v-flex>
+
+                <v-flex xs6>
+                    <v-card>
+                        <ImageUpload/>
+                    </v-card>
+                </v-flex xs6>
+
+                <v-flex xs6>
+                    <v-card>
+                        <Sticker_Theme_Selection/>
+                    </v-card>
+                </v-flex xs6>
+            </v-layout>
+        </v-container>
+    </v-app>
 </template>
 
 <script>
