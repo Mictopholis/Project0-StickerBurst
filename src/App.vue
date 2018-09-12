b24451d5a78e791378d4b93d31fce9d2ee96b14b
MeeraWakim <meera.wakim925@gmail.com>
2018-09-12 11:45:32 -0500

    getting rid of old portfolio elements, adding new site components

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,19 @@
 <template>
-    <v-app>
-
-        <v-toolbar flat>
-            <v-spacer></v-spacer>
-            <v-btn flat>ABOUT ME</v-btn>
-            <v-btn flat>PORTFOLIO</v-btn>
-            <v-btn flat>RESUME</v-btn>
-            <v-btn flat>CONTACT</v-btn>
-        </v-toolbar>
-
-        <v-jumbotron>
-            <v-container fill-height>
-                <v-layout align-center>
-                    <v-flex>
-                    <v-layout row wrap>
-                        <h3 class="display-3">Hi, I'm Meera.</h3>
-                </v-layout>
-                        <v-divider class="my-3"></v-divider>
-                    </v-flex>
-                </v-layout>
-            </v-container>
-        </v-jumbotron>
-
-        <v-flex>
-            <v-btn ref="button" color="primary" block @click="$vuetify.goTo('#element-near-top', { offset: -500 })">scroll</v-btn>
-        </v-flex>
-    </v-app>
+    
 </template>
 
 <script>
-import AboutMe from './components/AboutMe.vue'
-import * as easings from 'vuetify/es5/util/easing-patterns'
+import Login from './components/Login.vue'
+import ImageUpload from './components/ImageUpload.vue'
+import Sticker_Theme_Selection from './components/Sticker_Theme_Selection.vue'
+
 export default {
 
 name: 'App',
 components: {
-    AboutMe,
+    Login,
+    ImageUpload,
+    Sticker_Theme_Selection
 },
 data () {
       return {
@@ -47,7 +25,5 @@ data () {
 </script>
 
 <style>
-#element-near-top {
-  height: 100px;
-}
+
 </style>
