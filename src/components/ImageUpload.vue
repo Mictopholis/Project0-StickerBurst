960fdf4dd2575f32eeacda65e036fd5cf279b591
MeeraWakim <meera.wakim925@gmail.com>
2018-09-16 00:23:15 -0500

    set up image uplaod/display

--- src/components/ImageUpload.vue
+++ src/components/ImageUpload.vue
@@ -1,11 +1,61 @@
 <template>
-<h1>Image Upload</h1>
+<div id="app">
+  <div v-for="item in items">
+    <div v-if="!item.image">
+      <h2>Select an image</h2>
+      <input type="file" @change="onFileChange(item, $event)">
+    </div>
+    <div v-else>
+      <img :src="item.image" />
+      <button @click="removeImage(item)">Remove image</button>
+    </div>
+  </div>
+</div>
 </template>
 
 <script>
 
+   export default{
+  data: () => ({
+    items: [
+       {
+         image: false,
+       },
+
+    ],
+   }),
+  methods: {
+    onFileChange(item, e) {
+      var files = e.target.files || e.dataTransfer.files;
+      if (!files.length)
+        return;
+      this.createImage(item, files[0]);
+    },
+    createImage(item, file) {
+      var image = new Image();
+      var reader = new FileReader();
+
+      reader.onload = (e) => {
+        item.image = e.target.result;
+      };
+      reader.readAsDataURL(file);
+    },
+    removeImage: function (item) {
+      item.image = false;
+    }
+  }
+
+}
 </script>
 
 <style>
+#app {
+  text-align: center;
+}
+img {
+    height: 500px;
+}
+button {
 
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,12 +1,9 @@
 import Vue from 'vue'
 import Vuetify from 'vuetify'
 import App from './App.vue'
-import vueUpload from '@websanova/vue-upload'
 import 'vuetify/dist/vuetify.min.css'
 
 Vue.use(Vuetify)
-Vue.use(vueUpload)
-
 
 new Vue({
   el: '#app',
