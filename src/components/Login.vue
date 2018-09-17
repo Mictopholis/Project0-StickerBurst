bc8c030e800238557e7b4757bb1d8b30964d794f
Garrett Odom <garrettodom97@wireless-10-145-41-110.public.utexas.edu>
2018-09-17 08:43:49 -0500

    login changes

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,8 +1,77 @@
 <template>
-<h1>Login</h1>
+  
+  <div>
+  <v-layout row wrap>
+    <v-flex xs4>
+      <v-text-field
+              v-model="input.username"
+              label="Username"
+              hint="At least 8 characters"
+              counter
+              @click:append="show1 = !show1"
+      ></v-text-field>
+    </v-flex>
+
+    <v-spacer/>
+
+    <v-flex xs4>
+        <v-text-field
+                v-model="input.password"
+                :rules="[rules.required, rules.min]"
+                :type="show1 ? 'text' : 'password'"
+                position="relative"
+                name="input-10-1"
+                label="Password"
+                hint="At least 8 characters"
+                counter
+                @click:append="show1 = !show1"
+        ></v-text-field>
+    </v-flex>
+
+    <v-flex xs3>
+        <v-btn 
+                color="success"
+                v-on:click="login">Login
+        </v-btn>
+    </v-flex>
+
+  </v-layout>
+  </div>
+
 </template>
 
 <script>
+
+  export default {
+    data () {
+      return {
+        show1: false,
+        input: {
+          username: "",
+          password: ""
+        },
+        rules: {
+          required: value => !!value || 'Required.',
+          min: v => v.length >= 8 || 'Min 8 characters',
+        },
+        accounts: {
+          username: "garrett4",
+          password: "odomodom"
+        }
+      }
+    },
+    methods: {
+      login: function (event) {
+        if(this.input.username == this.accounts.username && this.input.password == this.accounts.password) {
+          alert('***Direct to higher access area***')
+        } else {
+          alert('Invalid Login Credentials. Please Try Again.')
+        }
+      }
+    }
+  }
+
+
 
 </script>
 
