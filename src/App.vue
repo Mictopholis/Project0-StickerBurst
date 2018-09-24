3c6e8a75a14cf0a8d7336cfaa39d61fd8ca958df
MeeraWakim <meera.wakim925@gmail.com>
2018-09-23 23:27:56 -0500

    deleted unecessary files

--- src/App.vue
+++ src/App.vue
@@ -196,19 +196,10 @@ Please upload an image first
 </template>
 
 <script>
-import Login from './components/Login.vue'
-import ImageUpload from './components/ImageUpload.vue'
-import Sticker_Theme_Selection from './components/Sticker_Theme_Selection.vue'
-
-
 export default {
 
 name: 'App',
-components: {
-    Login,
-    ImageUpload,
-    Sticker_Theme_Selection
-},
+
 data () {
       return {
         show1: false,
